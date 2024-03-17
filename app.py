a211b1c939ff54e64dd19bfccdaffd2f0c8c1a11
MobileSecure <163734133+MobileSecure@users.noreply.github.com>
2024-03-17 21:10:58 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st 
+
+st.markdown("""
+            # GrapheneOS
+            
+
+Introducing our GrapheneOS Security and Privacy Service:
+
+Easy, fast, reliable and secure installation of GrapheneOS within a day. 
+
+- Enhanced Privacy and Security with User Profiles: GrapheneOS offers advanced user profiles, providing a secure and private way to isolate different aspects of your digital life. 🔒
+- Prevent App Misbehavior: Separate profiles ensure that misbehaving or malicious apps cannot affect your main profile, keeping your data safe and secure. ⚠️
+- Easy Activation and Migration: Activating user profiles is simple with GrapheneOS settings, and you can effortlessly migrate existing apps to new profiles for added convenience and security. ✅
+- Protection of Private Data: Separate profiles are invaluable when sharing your phone with others, ensuring your private data remains protected at all times. 
+- Optimized Network and Wi-Fi Settings: We recommend specific network and Wi-Fi settings, such as turning off automatic Wi-Fi scanning and customizing hotspot names and passwords, to enhance security and privacy. 📶
+- Personalized Bluetooth and Device Settings: GrapheneOS allows you to customize device names and adjust Bluetooth settings for added privacy and convenience. 
+- Enhanced Security Measures: Disabling NFC and cloud printing can further bolster security and mitigate potential vulnerabilities. 🚫
+- Customized Notification and Display Settings: Fine-tuning notification settings, battery percentage display, and enabling dual Scopes ensure a personalized and secure user experience. 📵
+- Granular Privacy Controls: Manage sensor permissions, control camera and microphone access, and regulate clipboard visibility to safeguard your privacy effectively. 🔐
+- Tailored Security Measures: Choose between PIN and fingerprint security options based on your threat models and personal preferences, ensuring the highest level of protection for your device and data. 🔐
+- Experience the peace of mind that comes with GrapheneOS Security and Privacy Service - safeguarding your digital life with cutting-edge technology and unparalleled protection.
+
+### **GrapheneOS**
+
+The private and secure mobile operating system with Android app compatibility. Developed as a non-profit open source project.
+
+### Summary 
+
+GrapheneOS is like a supercharged shield for your smartphone, making it super secure and private. It's like having a bodyguard for your phone against hackers and snoops.
+
+Started in 2014, it was originally called CopperheadOS. But now, GrapheneOS focuses on making your phone extra safe by fixing lots of common problems that hackers might use to break in. It's like adding extra locks and alarms to your phone.
+
+When you use GrapheneOS, it's like your phone becomes a fortress. It's designed to keep your personal stuff safe, like photos, messages, and passwords, from prying eyes. Plus, it doesn't have any sneaky Google apps or services spying on you.
+
+And the best part? You don't need to be a tech whiz to use it. GrapheneOS tries to make things simple, so you can stay safe without having to worry about complicated settings or options. It's like having a tough bodyguard who works quietly in the background, keeping you safe without you even noticing.
+
+#### Which devices are **supported**?
+GrapheneOS has official production support for the following devices:
+
+- Pixel 8 Pro (husky)
+- Pixel Fold (felix)
+- Pixel 8 (shiba)
+- Pixel Tablet (tangorpro)
+- Pixel 7a (lynx)
+- Pixel 7 Pro (cheetah)
+- Pixel 7 (panther)
+- Pixel 6a (bluejay)
+- Pixel 6 Pro (raven)
+- Pixel 6 (oriole)
+- Pixel 5a (barbet)
+
+The following devices are end-of-life, no longer receive firmware or most driver security updates and receive extended support from GrapheneOS as part of the main releases with all GrapheneOS changes including all of the latest Android Open Source Project changes:
+
+- Pixel 5 (redfin)
+- Pixel 4a (5G) (bramble)
+
+The following devices are end-of-life, no longer receive firmware or driver security updates and receive extended support from GrapheneOS via a legacy branch based on Android 13 with only the Android Open Source Project security backports, certain other security patches and other minimal changes to keep them working:
+
+- Pixel 4a (sunfish)
+- Pixel 4 XL (coral)
+- Pixel 4 (flame)
+
+#### Summary
+Graphene OS has several important features that users may be missing out on if they stick to the default settings. We help our users to make sure that the right settings are in place.""")
